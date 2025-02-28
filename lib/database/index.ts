9f79899070fd82aa17c46b5e539b7cfa28a153df
Maneesh9 <amerpetamaneesh@gmail.com>
2025-02-28 13:51:29 +0530

    upadated mongoose api

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,15 +1,15 @@
 import { error } from 'console';
 import mongoose from 'mongoose';
 
-const MongoDB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null};
 export const connectToDatabse = async () => {
     if (cached.conn)  return cached.connect;
 
-    if(!MongoDB_URI) throw Error('MONGODB_URI is missing');
+    if(!MONGODB_URI) throw Error('MONGODB_URI is missing');
 
-    cached.promise=cached.promise || mongoose.connect(MongoDB_URI,{
+    cached.promise=cached.promise || mongoose.connect(MONGODB_URI,{
         dbName:'evently',
         bufferCommands:false,
     })
